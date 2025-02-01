8f2eb236282d8bad45d2b86e63e9e721fdd05939
Hyxlishious <98132601+Hykiinyte@users.noreply.github.com>
2025-01-31 19:40:38 -0500

    Update Begin.py

--- Begin.py
+++ Begin.py
@@ -1,1 +1,9 @@
+pip install robotpy
+pip install robotpy-rev
+pip install robotpy-ctre
+pip install robotpy-navx
+pip install robotpy-pathplanner
 
+import wpilib
+from wpilib.drive import MecanumDrive
+import rev  #for REV's SPARK MAX controllers(CAN)

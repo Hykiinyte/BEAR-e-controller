3c135d431c9efd76046b4e5f61f1a21e88a35ee2
Hyxlishious <98132601+Hykiinyte@users.noreply.github.com>
2025-01-31 20:20:47 -0500

    Create auto.py

---
+++ auto.py
@@ -0,0 +1,17 @@
+import wpilib
+
+class Autonomous:
+    def __init__(self, drivetrain):
+        self.timer = wpilib.Timer()
+        self.drivetrain = drivetrain
+
+    def start(self):
+        self.timer.reset()
+        self.timer.start()
+
+    def update(self):
+        """Example auto routine: Drive forward for 3 seconds"""
+        if self.timer.get() < 3.0:
+            self.drivetrain.drive_cartesian(0.5, 0, 0)  # Move forward
+        else:
+            self.drivetrain.drive_cartesian(0, 0, 0)  # Stop

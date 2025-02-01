126556a7a80151b5cd30b3a72c75ee98104582c1
Hyxlishious <98132601+Hykiinyte@users.noreply.github.com>
2025-01-31 20:23:20 -0500

    Update Teleop.py

--- subsystems/Teleop.py
+++ subsystems/Teleop.py
@@ -2,7 +2,7 @@ import wpilib
 
 class TeleopControl:
     def __init__(self, drivetrain):
-        self.controller = wpilib.Joystick(0)  # Joystick on port 0
+        self.controller = wpilib.Joystick(0)  #joystick from usb slot 0 (interchagable)
         self.drivetrain = drivetrain
 
     def update(self):

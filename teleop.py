2b0dece992c0519a04035e07babb48f695fdd862
Hyxlishious <98132601+Hykiinyte@users.noreply.github.com>
2025-01-31 20:19:59 -0500

    Create teleop.py

---
+++ teleop.py
@@ -0,0 +1,13 @@
+import wpilib
+
+class TeleopControl:
+    def __init__(self, drivetrain):
+        self.controller = wpilib.Joystick(0)  # Joystick on port 0
+        self.drivetrain = drivetrain
+
+    def update(self):
+        """Called every cycle during teleop"""
+        y_speed = -self.controller.getY()  # Forward/Backward
+        x_speed = self.controller.getX()  # Strafe
+        z_rotation = self.controller.getZ()  # Rotation
+        self.drivetrain.drive_cartesian(y_speed, x_speed, z_rotation)

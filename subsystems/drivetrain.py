09bba5c5f1f373d913f667d07d010e91241a112a
Hyxlishious <98132601+Hykiinyte@users.noreply.github.com>
2025-02-02 11:01:57 -0500

    Update drivetrain.py

--- subsystems/drivetrain.py
+++ subsystems/drivetrain.py
@@ -5,10 +5,10 @@ import rev  # For SPARK MAX
 class Drivetrain:
     def __init__(self):
         """Initialize drivetrain motors"""
-        self.front_left = rev.CANSparkMax(1, rev.CANSparkMax.MotorType.kBrushless)
-        self.front_right = rev.CANSparkMax(2, rev.CANSparkMax.MotorType.kBrushless)
-        self.rear_left = rev.CANSparkMax(3, rev.CANSparkMax.MotorType.kBrushless)
-        self.rear_right = rev.CANSparkMax(4, rev.CANSparkMax.MotorType.kBrushless)
+        self.front_left = rev.SparkMax(14, rev.SparkMax.MotorType.kBrushless)
+        self.front_right = rev.SparkMax(12, rev.SparkMax.MotorType.kBrushless)
+        self.rear_left = rev.SparkMax(16, rev.SparkMax.MotorType.kBrushless)
+        self.rear_right = rev.SparkMax(13, rev.SparkMax.MotorType.kBrushless)
 
         self.drive = wpilib.drive.MecanumDrive(
             self.front_left, self.rear_left, self.front_right, self.rear_right
@@ -17,3 +17,4 @@ class Drivetrain:
     def drive_cartesian(self, y_speed, x_speed, z_rotation):
         """Handles mecanum drive logic"""
         self.drive.driveCartesian(y_speed, x_speed, z_rotation)
+print("drivetrain initiated")

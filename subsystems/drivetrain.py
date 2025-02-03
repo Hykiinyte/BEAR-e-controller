1b06f7590abe28de13db1996b197573abed99d40
Hyxlishious <98132601+Hykiinyte@users.noreply.github.com>
2025-02-03 18:05:32 +0000

    welcome to my perfect math class

--- subsystems/drivetrain.py
+++ subsystems/drivetrain.py
@@ -3,24 +3,30 @@ import wpilib.drive
 import rev  # For SPARK MAX
 
 class Drivetrain:
-    def __init__(self):
-        """Initialize drivetrain motors"""
-        self.front_left = rev.SparkMax(14, rev.SparkMax.MotorType.kBrushless)
-        self.front_right = rev.SparkMax(12, rev.SparkMax.MotorType.kBrushless)
-        self.rear_left = rev.SparkMax(16, rev.SparkMax.MotorType.kBrushless)
-        self.rear_right = rev.SparkMax(13, rev.SparkMax.MotorType.kBrushless)
+    try:
+        def __init__(self):
+            """Initialize drivetrain motors"""
+            self.front_left = rev.SparkMax(14, rev.SparkMax.MotorType.kBrushless)
+            self.front_right = rev.SparkMax(12, rev.SparkMax.MotorType.kBrushless)
+            self.rear_left = rev.SparkMax(16, rev.SparkMax.MotorType.kBrushless)
+            self.rear_right = rev.SparkMax(13, rev.SparkMax.MotorType.kBrushless)
 
-        self.front_left.setInverted(False)
-        self.rear_left.setInverted(False)
-        self.front_right.setInverted(False)
-        self.rear_right.setInverted(False)
+            self.front_left.setInverted(False)
+            self.rear_left.setInverted(False)
+            self.front_right.setInverted(False)
+            self.rear_right.setInverted(False)
 
-        self.drive = wpilib.drive.MecanumDrive(
-            self.front_left, self.rear_left, self.front_right, self.rear_right
-        )
+            self.drive = wpilib.drive.MecanumDrive(
+                self.front_left, self.rear_left, self.front_right, self.rear_right
+            )
+    except:
+        print("Drivetrain failed.")
 
-    def drive_cartesian(self, y_speed, x_speed, z_rotation):
-        """Handles mecanum drive logic"""
-        self.drive.driveCartesian(y_speed, x_speed, z_rotation)
+    try:
+        def drive_cartesian(self, y_speed, x_speed, z_rotation):
+            """Handles mecanum drive logic"""
+            self.drive.driveCartesian(y_speed, x_speed, z_rotation)
+    except:
+        print("Mechanum drivetrain configuration failed.")
 
 print("drivetrain initiated")

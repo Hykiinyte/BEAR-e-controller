98293354d25a7dc0a70557387a9929a458bce581
Hyxlishious <98132601+Hykiinyte@users.noreply.github.com>
2025-02-12 21:08:07 -0500

    Update driverstation.py

--- subsystems/driverstation.py
+++ subsystems/driverstation.py
@@ -9,6 +9,6 @@ from commands.teleop import TeleopControl
 #idk use Tkinter to create a window and design a GUI for our driver station
 #while this is not necessary, it is a good utility to have, especially for debugging and reading sensor values in real time possibly
 root = tkinter.Tk()
-root.title("Driver Station")
+root.title("Cy-Bears Driver Station")
 root.geometry("400x400")
 root.configure(bg="black")

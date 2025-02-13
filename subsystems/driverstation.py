8722fdf84683d88b74906f96bdd737c271f33ce6
Hyxlishious <98132601+Hykiinyte@users.noreply.github.com>
2025-02-12 21:11:18 -0500

    Update driverstation.py

--- subsystems/driverstation.py
+++ subsystems/driverstation.py
@@ -12,3 +12,19 @@ root = tkinter.Tk()
 root.title("Cy-Bears Driver Station")
 root.geometry("400x400")
 root.configure(bg="black")
+
+#create a label for the title
+title = tkinter.Label(root, text="Cy-Bears Driver Station", font=("Open Sans", 24), bg="black", fg="white")
+title.pack()
+
+#create a label for the drivetrain
+drivetrain = tkinter.Label(root, text="Drivetrain", font=("Open Sans", 18), bg="black", fg="white")
+drivetrain.pack()
+
+#create a label for the utiliiy state
+util = tkinter.Label(root, text="Utility", font=("Open Sans", 18), bg="black", fg="white")
+util.pack()
+
+#create a label for the sensor values
+sensor = tkinter.Label(root, text="Sensor", font=("Open Sans", 18), bg="black", fg="white")
+sensor.pack()

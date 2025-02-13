ad2fdf00960a5be19e88707f6b641a6dd5027c62
Hyxlishious <98132601+Hykiinyte@users.noreply.github.com>
2025-02-12 21:06:24 -0500

    Update driverstation.py

--- subsystems/driverstation.py
+++ subsystems/driverstation.py
@@ -1,2 +1,14 @@
 import tkinter
-#idk
+
+import wpilib
+from subsystems.drivetrain import Drivetrain
+from subsystems.utilhandler import Utilhandler
+from commands.autonomous import Autonomous
+from commands.teleop import TeleopControl
+
+#idk use Tkinter to create a window and design a GUI for our driver station
+#while this is not necessary, it is a good utility to have, especially for debugging and reading sensor values in real time possibly
+root = tkinter.Tk()
+root.title("Driver Station")
+root.geometry("400x400")
+root.configure(bg="black")

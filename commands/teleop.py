f874540b5552889499985f245d87653000000043
Hyxlishious <98132601+Hykiinyte@users.noreply.github.com>
2025-02-15 17:07:58 -0500

    Update teleop.py

--- commands/teleop.py
+++ commands/teleop.py
@@ -3,15 +3,14 @@ import wpilib.drive
 
 class TeleopControl:
     def __init__(self, drivetrain):
-        self.controller = wpilib.Joystick(0)  # back to joystick baby
+        self.controller = wpilib.XboxController(0)  # nah actualy back to joystick
         self.drivetrain = drivetrain
 
     def update(self):
-        #ok should be axes 0 and 1 are the left stick and axes 3 and 4 are the right stick.
-        y_speed = -self.controller.getRawAxis(1)
-        x_speed = self.controller.getRawAxis(0)
+        y_speed = -self.controller.getLeftY()
+        x_speed = self.controller.getLeftX()
         x_speed_inv = x_speed * -1
-        z_rotation = self.controller.getRawAxis(4)  # Adjust the index as needed.
+        z_rotation = self.controller.getRightX()
         self.drivetrain.drive_cartesian(y_speed, x_speed_inv, z_rotation)
         #print(f"Teleop: y={y_speed}, x={x_speed}, rotation={z_rotation}")
 

a22e3b47656adfeb6c29f4aae3616cc6cce51ded
DODO499 <sanjoeaby@gmail.com>
2025-03-04 16:01:52 -0500

    Update utilhandler.py

--- subsystems/utilhandler.py
+++ subsystems/utilhandler.py
@@ -8,12 +8,6 @@ class Utilhandler:
         self.motor1 = rev.SparkMax(1, rev.SparkMax.MotorType.kBrushless)
         self.motor2 = rev.SparkMax(2 ,rev.SparkMax.MotorType.kBrushless)
 
-    def handle_utils(self):
-        """Handles tools like a ball intake or a shooter"""
-        self.operator = wpilib.Joystick(1)
-        self.motor1.set(self.operator.getY())
-        self.motor2.set(self.operator.getY())
-
 print("utils initiated")
 
 #This is incomplete code. It is meant to be used as a reference for the user to have something to start with.

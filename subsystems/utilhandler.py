aacef68624f8ea481043eeb91422bf5d9e2b2e3f
Hyxlishious <98132601+Hykiinyte@users.noreply.github.com>
2025-03-05 21:55:09 -0500

    Update utilhandler.py

--- subsystems/utilhandler.py
+++ subsystems/utilhandler.py
@@ -5,10 +5,24 @@ import rev  # For SPARK MAX
 class Utilhandler:
     def __init__(self):
         """Initialize utilitrain motors"""
-        self.motor1 = rev.SparkMax(1, rev.SparkMax.MotorType.kBrushless)
-        self.motor2 = rev.SparkMax(2 ,rev.SparkMax.MotorType.kBrushless)
+        self.motor1 = rev.SparkMax(11, rev.SparkMax.MotorType.kBrushless)
+        self.motor2 = rev.SparkMax(15, rev.SparkMax.MotorType.kBrushless)
 
 print("utils initiated")
+
+class Elevator:
+    def __init__(self, motor_id):
+        self.motor.setInverted(False)  # Adjust inversion as needed
+
+    def move(self, speed):
+        """Sets the elevator motor speed (-1 to 1)."""
+        self.motor.set(1)
+        print(f"Elevator moving at {1}")
+
+    def stop(self):
+        self.motor.set(0)
+        print("Elevator stopped")
+
 
 #This is incomplete code. It is meant to be used as a reference for the user to have something to start with.
 #Have fun coding!

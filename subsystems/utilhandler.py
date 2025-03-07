50190ff6c05f58cbd5b2f2129d8f9e53c6f845f4
DODO499 <sanjoeaby@gmail.com>
2025-03-07 12:21:37 -0500

    Update utilhandler.py

--- subsystems/utilhandler.py
+++ subsystems/utilhandler.py
@@ -7,8 +7,10 @@ class Utilhandler:
         """Initialize utilitrain motors"""
         global motor1
         global motor2
+        global motor3
         motor1 = rev.SparkMax(11, rev.SparkMax.MotorType.kBrushless)
         motor2 = rev.SparkMax(15, rev.SparkMax.MotorType.kBrushless)
+        motor3 = rev.SparkMax(1, rev.SparkMax.MotorType.kBrushless)
 
 print("utils initiated")
 
@@ -37,6 +39,19 @@ class CoralIntake:
     def stop(self):
         motor2.set(0)
         print("Coral intake stopped")
+
+class AlgaeIntake:
+    def __init__(self, motor_id):
+        motor3.setInverted(False)  # Adjust inversion as needed
+
+    def move(self, speed):
+        """Sets the algae intake motor speed (-1 to 1)."""
+        motor3.set(speed)
+        print(f"Algae intake moving at {speed}")
+
+    def stop(self):
+        motor3.set(0)
+        print("Algae intake stopped")
 
 
 #This is incomplete code. It is meant to be used as a reference for the user to have something to start with.
